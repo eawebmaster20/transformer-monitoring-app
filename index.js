7ccddccbf990908bdc0d7b6250d0ba7144f096c8
cypher20b <eaweb20@gmail.com>
2024-08-04 21:26:25 +0000

    feat: project initial setup and config

---
+++ index.js
@@ -0,0 +1,7 @@
+// const { SerialPort } = require('serialport')
+// const { DelimiterParser } = require('@serialport/parser-delimiter')
+
+
+// const test = new SerialPort({ path: '/dev/ttyUSB0', baudRate: 9600 });
+// const parser = test.pipe(new DelimiterParser({ delimiter: '\n' }))
+// parser.on('data', (dat)=>console.log("Recieved: '" +dat +"' from arduino"))
---
+++ preload.js
@@ -0,0 +1,16 @@
+window.addEventListener('DOMContentLoaded',()=>{
+    const replaceText = (selector, text)=>{
+        const element = document.getElementById(selector)
+        if (element) {
+            element.innerText = text
+        }
+        for (const dependency of ['chrome','node', 'electron']) {
+            replaceText(`${dependency}-version`,process.versions[dependency])
+        }
+    }
+    let counter = 0
+    document.getElementById('add').addEventListener('click',()=>{
+        counter++;
+        document.getElementById('spanLabel').innerText = counter
+    })
+})
