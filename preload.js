7d9909d2b30dd18688af037e3008bdf34e41c403
Emmanuel Adomako <102923937+eawebmaster20@users.noreply.github.com>
2025-01-17 19:00:12 +0000

    Update preload.js with socket.io-client setup

--- preload.js
+++ preload.js
@@ -1,6 +1,5 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
-
 // Expose Electron APIs to the renderer process
 contextBridge.exposeInMainWorld('serialAPI', {
   listPorts: () => ipcRenderer.invoke('list-ports'),
@@ -8,9 +7,14 @@ contextBridge.exposeInMainWorld('serialAPI', {
   startReading: (delimiter) => ipcRenderer.send('start-reading', delimiter),
   onSerialData: (callback) => ipcRenderer.on('serial-data', (event, data) => callback(data)),
   onDisconnect: (callback) => ipcRenderer.on('serial-disconnect', (event) => callback(event)),
-
+  
   startPortMonitoring: () => ipcRenderer.send('start-port-monitoring'),
   stopPortMonitoring: () => ipcRenderer.send('stop-port-monitoring'),
   onPortAvailable: (callback) => ipcRenderer.on('port-available', (event, port) => callback(port)),
   onPortUnavailable: (callback) => ipcRenderer.on('port-unavailable', (event, port) => callback(port)),
+  fbadmin: () => ipcRenderer.invoke('init-fb'),
+  
+  connectToServer: () => ipcRenderer.invoke('connect-to-server'),
+  sendMessage: (message) => ipcRenderer.invoke('send-message', message),
+  disconnectFromServer: () => ipcRenderer.invoke('disconnect-from-server'),
 });
